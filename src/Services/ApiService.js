c757c3c5b48bfd15a7df3cf16e8c7447b07907fe
Eli <elionlink@gmail.com>
2020-07-28 18:26:48 +0300

    280720

--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -1,6 +1,6 @@
 export const apiRates = async () => {
   const url =
-    'http://apilayer.net/api/live?access_key=3c81786f9b3d2e267f40d08af97b97f2&currencies=usd,ils,eur,btc,thb';
+    'https://apilayer.net/api/live?access_key=72b64c79ceca583e1bd96ad83c49c47b&currencies=usd,ils,eur,btc,thb';
   let resp = await fetch(url);
   let data = await resp.json();
   return data;
